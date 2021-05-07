672ac0f5045bfac296e7b657eb3847e0428409e8
Michał Zagojski <zagojski.michal.99@gmail.com>
2021-05-07 16:20:07 +0200

    Add bottom tags generation, and left aside list sorting

--- js/script.js
+++ js/script.js
@@ -27,6 +27,34 @@ function titleClickHandler(event){
   article.classList.add('active');
 }
 
+function tagClickHandler(event){
+  /* prevent default action for this event */
+  event.preventDefault();
+  /* make new constant named "clickedElement" and give it the value of "this" */
+  const clickedElement = this;
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+  const href = clickedElement.getAttribute('href');
+  /* make a new constant "tag" and extract tag from the "href" constant */
+  const tag = href.replace('#tag-', '');
+  /* find all tag links with class active */
+  const activeLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+  /* START LOOP: for each active tag link */
+  for(let activeLink of activeLinks){
+    /* remove class active */
+    activeLink.classList.remove('active');
+  /* END LOOP: for each active tag link */
+  }
+  /* find all tag links with "href" attribute equal to the "href" constant */
+  const equalTags = document.querySelectorAll('[href^="' + href + '"]');
+  /* START LOOP: for each found tag link */
+  for(let equalTag of equalTags){
+    /* add class active */
+    equalTag.classList.add('active');
+  /* END LOOP: for each found tag link */
+  }
+  /* execute function "generateTitleLinks" with article selector as argument */
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+}
 
 
 const optArticleSelector = '.post',
@@ -34,13 +62,13 @@ const optArticleSelector = '.post',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list';
 
-function generateTitleLinks(){
+function generateTitleLinks(customSelector = ''){
   /* [DONE] remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML='';
   /* [DONE] for each article */
   let html = '';
-  const articles = document.querySelectorAll(optArticleSelector);
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
   for(let article of articles){
     /* [DONE] get the article id */
     const articleId = article.getAttribute('id');
@@ -52,15 +80,14 @@ function generateTitleLinks(){
     html = html + linkHTML;
   }
   titleList.innerHTML = html;
-}
-
-generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+  }
 }
+generateTitleLinks();
 
 function generateTags(){
   /* find all articles */
@@ -88,5 +115,23 @@ function generateTags(){
   /* END LOOP: for every article: */
   }
 }
-
 generateTags();
+
+function generateAuthors(){
+  const articles = document.querySelectorAll('.post');
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+  const tagLinks = document.querySelectorAll('.post-tags a');
+  /* START LOOP: for each link */
+  for(let tagLink of tagLinks){
+    /* add tagClickHandler as event listener for that link */
+    tagLink.addEventListener('click', tagClickHandler);
+  /* END LOOP: for each link */
+  }
+}
+
+addClickListenersToTags();
+
+
